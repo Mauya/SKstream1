5c72d064c6a7f05806d7c16cd08898bce204dcc3
Mauya <smkadungure@live.co.uk>
2017-03-21 20:28:43 +0000

    amend contact js and html

--- js/controllers/contactController.js
+++ js/controllers/contactController.js
@@ -1,5 +1,13 @@
 angular.module('RouteControllers', [])
-app.controller("contactController", function($scope){
+app.controller('contactController', function($scope, $http) {
+  $scope.formData = {};
+  $scope.processForm = function() {
+    alert('valid form!')
+    $http({
+      method  : 'POST',
+      data    : $scope.formData
+    });
+  };
   var contact = {
     address:"22 Old Lane, Dublin, Ireland TY6 LP16,",
     email:"swingcrew@sc.ie",
