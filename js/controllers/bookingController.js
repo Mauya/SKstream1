4bd1cfa443f98d823ee47cfc96d4230fa00c09d4
Mauya <smkadungure@live.co.uk>
2017-03-28 23:10:12 +0100

    Booking form initiated

---
+++ js/controllers/bookingController.js
@@ -0,0 +1,57 @@
+angular.module("RouteControllers", []);
+app.controller("bookingController", ["$scope", function($scope){
+	$scope.booking={Title:"For Special Events Make a Booking Here!"};
+
+
+
+      this.selectEvent = function(setEvent) {
+        if (!setEvent.active) {
+          angular.forEach(this.events, function(s) {
+            s.active = false;
+          });
+          setEvent.active = true;
+        }
+
+        total();
+      };
+
+
+      // Datepicker
+
+      $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened1 = true;
+        $scope.opened2 = false;
+      };
+
+      $scope.open2 = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened1 = false;
+        $scope.opened2 = true;
+      };
+
+      $scope.clear = function() {
+        $scope.dt = null;
+        $scope.dt2 = null;
+      };
+
+      $scope.toggleMin = function() {
+        $scope.minDate = $scope.minDate ? null : new Date();
+      };
+      $scope.toggleMin();
+
+      $scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1
+      };
+
+      $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+      $scope.format = $scope.formats[0];
+
+    }
+  ]);
+
--- js/controllers/contactController.js
+++ js/controllers/contactController.js
@@ -3,29 +3,21 @@ app.controller('contactController', function($scope){
 $scope.contacts =[
  {
   department:"General inquires",
-  address:"22 Old Lane, Dublin, Ireland TY6 LP16,",
-  name: "Reception",
   email:"general@swingcrew.ie",
   telephone: "xx xxx xxxx"
   },
   {
   department:"Management",
-  address:"22 Old Lane, Dublin, Ireland TY6 LP16,",
-  name:"Tracy Mucha",
 	email:"info@swingcrew.ie",
 	telephone: "xx xxx xxxx"
   },
   {
   department:"Booking",
-  address:"22 Old Lane, Dublin, Ireland TY6 LP16,",
-  name:"Brookyln Jones",
 	email:"booking@swingcrew.ie",
 	telephone: "xx xxx xxxx"
   },
   {
   department:"Public Relations",
-  address:"22 Old Lane, Dublin, Ireland TY6 LP16,",
-  name:"Savanna Shaw",
 	email:"pr@swingcrew.ie",
 	telephone: "xx xxx xxxx"
   }
