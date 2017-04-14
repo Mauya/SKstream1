06ae57b6c71da0165f413ec6c94c46108c251636
Mauya <smkadungure@live.co.uk>
2017-04-14 09:58:49 +0100

    styling index page

--- js/controllers/photoController.js
+++ js/controllers/photoController.js
@@ -1,10 +1,10 @@
 app.controller('photoController', function ($scope) {
   $scope.images = [
-  	 {image: 'images/photos/photo1.jpg'},
-	 {image: 'images/photos/photo2.jpg'},
-	 {image: 'images/photos/photo3.jpg'},
-	 {image: 'images/photos/photo4.jpg'},
-	 {image: 'images/photos/photo5.jpg'}
+  	 {'img':'images/photos/photo1.jpg'},
+	 {'img': 'images/photos/photo2.jpg'},
+	 {'img': 'images/photos/photo3.jpg'},
+	 {'img': 'images/photos/photo4.jpg'},
+	 {'img': 'images/photos/photo5.jpg'}
   ];
   
  });
