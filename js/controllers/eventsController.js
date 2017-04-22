5d6d149806a13dde5da906fa2145923512e695ad
Mauya <smkadungure@live.co.uk>
2017-04-22 18:46:44 +0100

    Fatal styling of about page

--- js/controllers/eventsController.js
+++ js/controllers/eventsController.js
@@ -8,7 +8,7 @@ app.controller('eventsController', ['$scope', function($scope) {
 	  }, 
 	  { 
 	    image: 'images/events/corporate1.jpg',	     
-	    title: 'Corporate Events', 
+	    title: 'Corporate Galas', 
 	    button:  'Book Now' 
 	  },
 	  {
@@ -24,20 +24,38 @@ app.controller('eventsController', ['$scope', function($scope) {
 	];
 
 	$scope.reviewtitle = 'What Our Fans are Saying!';    
-    $scope.myInterval = 3000;
-    $scope.slides= [
-      {
+    $scope.myInterval = 5000;
+    $scope.reviews= [
+      { 
+      	id: '0',
         review: "The band is the best, I was so satisfied with our wedding reception!",
         fan: "Will Stone"
       },
       {
+      	id: '1',
         review: "Amazing staging and awesome Music!",
         fan: "Peter Roberts"
       },
       {
+      	id: '2',
         review: "Wow ...you have to experience the Swing Crew to say wow!",
         fan: "Jackie Todd"
 
+      },
+      {
+      	id: '3',
+        review: "Amazing lighting and sound out of the charts!",
+        fan: "George Martin"
+      },
+      {
+      	id: '4',
+        review: "Engaging band members, What a talent",
+        fan: "Anna Bush"
+      },
+      {
+      	id: '5',
+        review: "Great reception and positive vibs",
+        fan: "Hunter Mooja"
       }
     ];
 }]);
