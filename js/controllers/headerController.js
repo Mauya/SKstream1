8b80a7cb8b517482b4fa04bd4c73a826cb711154
Mauya <smkadungure@live.co.uk>
2017-05-02 20:59:36 +0100

    updated home and header controllers

---
+++ js/controllers/headerController.js
@@ -0,0 +1,9 @@
+app.controller('headerController', ['$scope', '$location', '$route', function($scope, $location, $route) 
+{ 
+    $scope.isActive = function (viewLocation) {
+     var active = (viewLocation === $location.path());
+     return active;
+    }
+}]);
+
+
---
+++ js/controllers/homeController.js
@@ -0,0 +1,39 @@
+app.controller('homeController', ['$scope', function($scope) {
+  $scope.title = "Swing Crew Events";
+  $scope.title1 = "This is Our Music - Welcome";
+  $scope.myInterval = 5000;
+  $scope.noWrapSlides=false;
+  $scope.active = 0;
+  $scope.slides= [
+    {
+      image: 'images/home/swing1.jpg',
+      id: 0,
+      caption: 'Social event'
+    },
+    {
+      image: 'images/home/swing2.jpg',
+      id: 1,
+      caption: 'Social event'
+    },
+    {
+      image: 'images/home/swing3.jpg',
+      id:2,
+      caption: 'Social event'
+    },
+    {
+      image: 'images/home/swing4.jpg',
+      id:3,
+      caption: 'Corporate function'
+    },
+    {
+      image: 'images/home/swing5.jpg',
+      id: 4,
+      caption: 'Corporate function'
+    },
+    {
+      image: 'images/home/swing6.jpg',
+      id: 5,
+      caption: 'Wedding function'
+    }
+  ];
+}]);
