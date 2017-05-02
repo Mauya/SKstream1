1ee4e019a7c01b3390290325fc5022d11b865b91
Mauya <smkadungure@live.co.uk>
2017-05-02 22:51:58 +0100

    eventinfo altered

--- js/controllers/eventsController.js
+++ js/controllers/eventsController.js
@@ -1,5 +1,4 @@
 app.controller('eventsController', ['$scope', function($scope) {
-  $scope.events = {heading:"Our Services"};
   $scope.events = [ 
 	  { 
 	    image: 'images/events/wedding1.jpg',	    
