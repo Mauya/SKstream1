8548d8ce47c4ec994506d4b2252eb3350f34b62b
Mauya <smkadungure@live.co.uk>
2017-05-03 18:31:35 +0100

    header made a directive

---
+++ js/directives/header.js
@@ -0,0 +1,10 @@
+app.directive('header', function() { 
+  return { 
+    restrict: 'E', 
+    scope: { 
+      info: '=' 
+    },
+    templateUrl: 'js/directives/header.html',
+    controller: "headerController"
+  }; 
+});
